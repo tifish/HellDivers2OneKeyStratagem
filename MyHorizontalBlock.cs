114bfd384b77fdbd72cc582dc956a7b90add66b0
Tinyfish <tifish@gmail.com>
2024-06-25 22:53:23 +0800

    Use more custom control layout.

--- MyHorizontalBlock.cs
+++ MyHorizontalBlock.cs
@@ -8,9 +8,11 @@ namespace HellDivers2OneKeyStratagem;
 [ContentProperty(nameof(Children))]
 public class MyHorizontalBlock : UserControl
 {
+    private readonly Border _border;
+
     public MyHorizontalBlock()
     {
-        var border = new Border
+        _border = new Border
         {
             Padding = new Thickness(20, 10, 20, 10),
             Margin = new Thickness(0, 1, 0, 1),
@@ -18,15 +20,15 @@ public class MyHorizontalBlock : UserControl
             VerticalAlignment = VerticalAlignment.Top,
             HorizontalAlignment = HorizontalAlignment.Stretch,
         };
-        border.SetResourceReference(Border.BackgroundProperty, "CardBackgroundFillColorDefaultBrush");
-        Content = border;
+        _border.SetResourceReference(Border.BackgroundProperty, "CardBackgroundFillColorDefaultBrush");
+        Content = _border;
 
         var simpleStackPanel = new SimpleStackPanel
         {
             Orientation = Orientation.Horizontal,
             Spacing = 10,
         };
-        border.Child = simpleStackPanel;
+        _border.Child = simpleStackPanel;
 
         Children = simpleStackPanel.Children;
     }
@@ -42,4 +44,26 @@ public class MyHorizontalBlock : UserControl
         typeof(UIElementCollection),
         typeof(MyHorizontalBlock),
         new PropertyMetadata());
+
+    public bool HasBackground
+    {
+        get => (bool)GetValue(HasBackgroundProperty);
+        set => SetValue(HasBackgroundProperty, value);
+    }
+
+    public static readonly DependencyProperty HasBackgroundProperty = DependencyProperty.Register(
+        nameof(HasBackground),
+        typeof(bool),
+        typeof(MyHorizontalBlock),
+        new PropertyMetadata(true, OnHasBackgroundChanged));
+
+    private static void OnHasBackgroundChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (MyHorizontalBlock)d;
+        var hasBackground = (bool)e.NewValue;
+        if (hasBackground)
+            control._border.SetResourceReference(Border.BackgroundProperty, "CardBackgroundFillColorDefaultBrush");
+        else
+            control._border.ClearValue(Border.BackgroundProperty);
+    }
 }
