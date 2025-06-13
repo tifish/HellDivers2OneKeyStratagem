6b8fc6c66f4ad12c17e9e29347c8c75be1d9116e
Tinyfish <tifish@gmail.com>
2025-06-13 23:26:07 +0800

    Resize window correctly.

--- MainWindow/MainWindow.axaml.cs
+++ MainWindow/MainWindow.axaml.cs
@@ -19,6 +19,10 @@ public partial class MainWindow : Window
         try
         {
             InitializeComponent();
+
+#if DEBUG
+            this.AttachDevTools();
+#endif
         }
         finally
         {
@@ -48,19 +52,25 @@ public partial class MainWindow : Window
 
         // Add the window height to show more content
         var desktopRect = Screens.Primary.WorkingArea.ToRect(DesktopScaling);
+        var windowHeight = FrameSize!.Value.Height;
 
+        // If not all stratagems are visible, add the height of the missing stratagems.
+        // But not more than the height of the desktop.
         if (StratagemsStackPanel.Bounds.Height > StratagemsScrollViewer.Bounds.Height)
         {
             var diff1 = StratagemsStackPanel.Bounds.Height - StratagemsScrollViewer.Bounds.Height;
-            var diff2 = desktopRect.Height - Bounds.Height;
+            // If the window is too tall, diff2 can be negative, which will reduce the height to the desktop.
+            var diff2 = desktopRect.Height - windowHeight;
             StratagemsScrollViewer.Height += Math.Min(diff1, diff2);
         }
 
-        // Center the window
-        var pixelRect = new PixelRect(Position, PixelSize.FromSize(Bounds.Size, DesktopScaling));
-        Position = Screens.Primary.WorkingArea.CenterRect(pixelRect).Position;
+        // Center the window after the height is set.
+        Dispatcher.UIThread.Post(() =>
+        {
+            var pixelRect = new PixelRect(Position, PixelSize.FromSize(FrameSize!.Value, DesktopScaling));
+            Position = Screens.Primary.WorkingArea.CenterRect(pixelRect).Position;
+        });
     }
-
 
     private MainViewModel Model => (MainViewModel)DataContext!;
 }
